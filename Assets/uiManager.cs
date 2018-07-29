6ce03f323e19bd71c98332dc813a3a15d954cae4
faruk18 <mdmishu247@gmail.com>
2018-07-29 17:16:55 +0600

    first Commit

---
+++ Assets/uiManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiManager : MonoBehaviour {
+
+	public Button[] buttons;
+	public Text scoreText;
+	bool gameover;
+	int score;
+	// Use this for initialization
+	void Start () {
+		gameover = false;
+		score = 0;
+		InvokeRepeating ("scoreUpdate", 1.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Score: " + score;
+
+		
+	}
+
+	void scoreUpdate(){
+		if (gameover == false) {
+			score += 1;
+		}
+	}
+
+	public void gamoverActivated() {
+		gameover = true;
+		foreach (Button button in buttons) {
+			button.gameObject.SetActive (true);
+		}
+	}
+
+	public void Play() {
+		Application.LoadLevel ("level1");
+	}
+	public void Pause (){
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+		}
+		else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+		}
+	}
+
+   
+	public void Menu() {
+
+		Application.LoadLevel ("Menu");
+	}
+
+	public void Exit (){
+
+		Application.Quit ();
+	}
+
+
+}
+
